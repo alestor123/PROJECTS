9ceebb6b0fbd58814c602413c7b66fdcca7aa8bb
alestor123 <alestoraldous@gmail.com>
2022-08-01 15:07:46 +0530

    init code

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align=center>DEVENT-FETCH</h1>
+> This is a simple code for fetching latest repos and projects (microservice)
+
+<p align=center>
+<img src="https://img.shields.io/github/license/alestor123/DEVENT-FETCH" alt=issues >
+<a href="https://github.com/alestor123/DEVENT-FETCH/issues">
+<img src="https://img.shields.io/github/issues-raw/alestor123/DEVENT-FETCH"></a>
+<img src="https://github.com/alestor123/DEVENT-FETCH/actions/workflows/main.yml/badge.svg?branch=master">
+</p>
+
+## 🚀 Usage
+### With [vercel](https://now.sh)
+```
+$ git clone https://github.com/alestor123/DEVENT-FETCH.git
+$ vercel deploy DEVENT-FETCH/ --env GHTOKEN=xxx
+```
+
+## Environment variables
+
+Define the following environment variables:
+
+- `GHTOKEN` - [Personal access token.](https://github.com/settings/tokens/new)
+
+# Screenshots    
+
---
+++ api/v1/projects.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const events = require('../../data/json/projects.json')
+const axios = require('axios')
+const controlAccess = require('control-access')
+const token = process.env.GHTOKEN
+const username = process.env.GHUSERNAME
+const number = process.env.MAXNUMBER || 50
+const { readFileSync, read } = require('fs')
+const { resolve } = require('path')
+
+const api = 'https://api.github.com/graphql'
+if (token) axios.defaults.headers.common.Authorization = `bearer ${token}`
+
+module.exports = async (req, res) => {
+  controlAccess()(req, res)
+  try {
+    res.setHeader('content-type', 'application/json')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('cache-control', 's-maxage=86400, max-age=0')
+    const data = (await axios.post(api,
+      {
+        query:
+        `
+        query ($cursor: String) {
+            user(login: "${req.query.username || username}") {
+                repositories(
+                    last: ${req.query.maxnumber || number},
+                    isLocked: false,
+                    ownerAffiliations: OWNER,
+                    privacy: PUBLIC,
+                    orderBy: {
+                        field: CREATED_AT,
+                        direction: ASC
+                    }
+                    before: $cursor
+                ) {
+                    edges {
+                        node {
+                            name
+                            description
+                            url
+                            primaryLanguage {
+                                name
+                                color
+                            }
+                            forks {
+                                totalCount
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `,
+        variables: { login: req.query.username }
+      })).data
+
+    res.json({
+      latestRepos: data.data.user.repositories.edges.filter(({ node: repo }) => repo.description && repo.name !== '.github').map(repo => repo.node).reverse(),
+      projects: Object.values(JSON.parse(readFileSync(resolve('data/json/projects.json'), 'utf-8'))) // fetch info from gh api
+    })
+    // console.log(resolve('data/json/projects.json'))
+  } catch (error) {
+    console.error(error)
+    res.statusCode = 500
+    res.setHeader('content-type', 'text/plain')
+    res.end('Internal server error')
+  }
+}
